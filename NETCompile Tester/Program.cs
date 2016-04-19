1b1e75e623b66328adc5e36320c30735f0a3bf02
ion <ionix@protonmail.com>
2016-04-19 20:27:17 +0200

    Compiler 1 (C#5 and VB) working now.

--- NETCompile Tester/Program.cs
+++ NETCompile Tester/Program.cs
@@ -11,6 +11,7 @@ namespace NETCompile_Tester
     {
         static void Main(string[] args)
         {
+            Compiler1.Compile("HelloWorld.exe", Compiler1.Target.EXE, Compiler1.Platform.x86, new string[] { "",""}, new string[] { "", "" }, false, @"C:\Users\Luca\Desktop\helloworld.cs", Compiler1.Language.CSharp);
         }
     }
 }
--- NETCompile/Compiler.cs
+++ NETCompile/Compiler.cs
@@ -5,22 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Diagnostics;
 
 namespace NETCompile
 {
-    public class Compiler
+    public class Compiler1  //for CSharp5 use Roslyn for CSharp6
     {
         public enum Target
         {
-            DLL, EXE
+            DLL,
+            EXE
         }
 
-        public static void Compile(string asm, Target t)
+        public enum Platform
+        {
+            x86,
+            x64
+        }
+
+        public enum Language
+        {
+            CSharp,
+            VB
+        }
+
+        public static void Compile(string asmName, Target t, Platform p, string[] pathToReferences, string[] pathToRessources, bool allowUnsafeCode, string sourcePath, Language l)
         {
             //get the csc.exe location
             string frameworkPath = RuntimeEnvironment.GetRuntimeDirectory();
-            string cscPath = Path.Combine(frameworkPath, "csc.exe");
+            string cscPath = "";
             string targetOption = "";
+            string platformOption = "";
+            string references = "";
+            string ressources = "";
+
+            string preAsm = "/out:";
+            string preTarget = " /t:";
+            string prePlatform = " /platform:";
+            string preRef = " /r:";
+            string preRes = " /res:";
+            string preAllowUnsafe = " /unsafe";
 
             switch(t)
             {
@@ -31,6 +55,76 @@ namespace NETCompile
                     targetOption = "library";
                     break;
             }
+
+            switch (p)
+            {
+                case Platform.x86:
+                    platformOption = "x86";
+                    break;
+                case Platform.x64:
+                    platformOption = "x64";
+                    break;
+            }
+
+            switch (l)
+            {
+                case Language.CSharp:
+                    Path.Combine(frameworkPath, "csc.exe");
+                    break;
+                case Language.VB:
+                    Path.Combine(frameworkPath, "vbc.exe");
+                    break;
+            }
+
+            if (pathToReferences[0] == "")
+            {
+                preRef = " "; //no references
+            }
+            else
+            {
+                foreach(string line in pathToReferences)
+                {
+                    if (line == pathToReferences[pathToReferences.Count() - 1])
+                    {
+                        references += line;
+                    }
+                    else
+                    {
+                        references += line + ",";
+                    }
+                }
+            }
+
+            if (pathToRessources[0] == "")
+            {
+                preRes = " "; //no ressources
+            }
+            else
+            {
+                foreach (string line in pathToRessources)
+                {
+                    if (line == pathToRessources[pathToRessources.Count() - 1])
+                    {
+                        ressources += line;
+                    }
+                    else
+                    {
+                        ressources += line + ",";
+                    }
+                }
+            }
+
+            if(allowUnsafeCode)
+            {
+                preAllowUnsafe += "+";
+            }
+            else
+            {
+                preAllowUnsafe += "-";
+            }
+
+            File.WriteAllText("compile.bat", "\""+cscPath+"\" "+preAsm + asmName + prePlatform + platformOption +preTarget + targetOption + preRef + references + preRes + ressources + preAllowUnsafe + " \"" + sourcePath + "\"" + "\n pause");
+            Process.Start("compile.bat");
         }
     }
 }
